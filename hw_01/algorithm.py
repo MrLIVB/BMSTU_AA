ecfe965ba4fda1402aa0ba8de245e01e4bce83c4
kaa18u719 <ya.antonkuzin@yandex.ua>
2020-11-25 13:57:41 +0300

    Remove extra strings in hw_01\algorithm.py

--- hw_01/algorithm.py
+++ hw_01/algorithm.py
@@ -1,26 +1,4 @@
-def temp_func(matr, rows, cols):
-    avg_positive = 0
-    pos_cnt = 0
-    avg_negative = 0
-    neg_cnt = 0
-    for i in range(rows):
-        for j in range(cols):
-            if matr[i][j] > 0:
-                avg_positive = avg_positive + matr[i][j]
-                pos_cnt += 1
-            elif matr[i][j] < 0:
-                avg_negative = avg_negative + matr[i][j]
-                neg_cnt += 1
-    if pos_cnt:
-        avg_positive /= pos_cnt
-    if neg_cnt:
-        avg_negative /= neg_cnt
-    if pos_cnt > neg_cnt:
-        return avg_positive
-    else:
-        return avg_negative
-
-def temp_func2(matr, rows, cols):
+def alg(matr, rows, cols):
     avg = 0                         # 1
     avg_cnt = 0                     # 2
     for i in range(rows):
@@ -42,4 +20,4 @@ def temp_func2(matr, rows, cols):
 
 
 matr = [[30, -12, 3, -21], [-3, -5, -6, 2], [-21, -21, -1, 0]]
-print(temp_func2(matr, 3, 4))
+print(alg(matr, 3, 4))
